835816f84abd81cab15f803e702902ee99f5762a
Aaron Weimann <aaron.weimann@uni-duesseldorf.de>
2014-10-24 16:52:40 +0200

    added an option to the discretize_likelihood_recon script that allows to restrict discretization to the phenotypes

--- discretize_likelihood_recon.py
+++ discretize_likelihood_recon.py
@@ -4,25 +4,30 @@ import pandas
 import sys
 import numpy as np
 
-def single_matrix(m, f,  t, outdir):
+def single_matrix(m, f,  t, discretize_pt_only, outdir):
     """discretize a single matrix""" 
     print "processing file %s"%(f)
-    m[m>=t] = 1
-    m[m<t] = 0
+    if discretize_pt_only:
+        m.loc[m.iloc[:, m.shape[1] - 1] >= t, m.shape[1]] = 1
+    else: 
+        m[m >= t] = 1
     s = m.shape
+    #drop edges / samples that do not exceed the phenotype threshold 
     m = m[~((m.iloc[:,m.shape[1] - 1] != 0) & (m.iloc[:,m.shape[1] - 1] < t))]
-    print "there are %s cases with probable gain and loss event for the same edge" % (m > 1).sum().sum()
+    if not discretize_pt_only:
+        m[m < t] = 0
+        print "there are %s cases with probable gain and loss event for the same edge" % (m > 1).sum().sum()
     print "there are %s probable phenotype events" % (m.iloc[:, m.shape[1] - 1] >= 1).sum()
     print "there were %s samples removed due to unclear status" % (s[0] - m.shape[0])
     m.to_csv(os.path.join(outdir, f), sep="\t", header=None, index=True)
     return m
 
-def threshold_matrix(dir1, t, outdir, loss_dir2=None, is_internal = False):
+def threshold_matrix(dir1, t, outdir, loss_dir2=None,discretize_pt_only = False ,is_internal = False, ):
     """discretize one or combine two matrices into one discretized matrix"""
     if loss_dir2 is None:
         for f1 in os.listdir(dir1):
             m1 = pandas.read_csv(os.path.join(dir1, f1), sep="\t", index_col=0, header=None) 
-            m =  single_matrix(m1, f1, t, outdir)
+            m =  single_matrix(m1, f1, t,  discretize_pt_only, outdir)
             if is_internal:
                 return m
 
@@ -34,7 +39,7 @@ def threshold_matrix(dir1, t, outdir, loss_dir2=None, is_internal = False):
             else: 
                 #no gain event file found, go into single matrix case
                 m2 = pandas.read_csv(os.path.join(loss_dir2, m_f), sep="\t", index_col=0, header=None) 
-                m =  single_matrix(m2, m_f, t, outdir)
+                m =  single_matrix(m2, m_f, t,  discretize_pt_only, out_dir)
                 if is_internal:
                     return m
                 continue 
@@ -42,7 +47,7 @@ def threshold_matrix(dir1, t, outdir, loss_dir2=None, is_internal = False):
                 m2 = pandas.read_csv(os.path.join(loss_dir2, m_f), sep="\t", index_col=0, header=None) 
             else: 
                 #no loss event file found, go into single matrix case
-                m = single_matrix(m1, m_f, t, outdir)
+                m = single_matrix(m1, m_f, t, discretize_pt_only,  outdir)
                 if is_internal:
                     return m
                 continue
@@ -50,14 +55,18 @@ def threshold_matrix(dir1, t, outdir, loss_dir2=None, is_internal = False):
             #drop condition for m1 where the pt is non-zero but does not exceed the threshold
             drop_m1 = ~((m1.iloc[:,m1.shape[1] - 1] != 0) & (m1.iloc[:,m1.shape[1] - 1] < t)) 
             #set all elements greater or equal the threshold to 1
-            m1[m1>=t] = 1
-            #set all elements smaller than the threshold to 0 
-            m1[m1<t] = 0
+            if discretize_pt_only:
+                m1.loc[m1.iloc[:, m1.shape[1] - 1] >= t, m1.shape[1]] = 1
+                m2.loc[m2.iloc[:, m2.shape[1] - 1] >= t, m2.shape[1]] = 1
+                m = m1 + (1 - m1)*m2 
+            else:
+                m1[m1>=t] = 1
+                m1[m1<t] = 0
+                m2[m2>=t] = 1
+                m2[m2<t] = 0
+                m = m1 + m2
             #drop condition for m2
             drop_m2 = ~((m2.iloc[:,m2.shape[1] - 1] != 0) & (m2.iloc[:,m2.shape[1] - 1] < t)) 
-            m2[m2>=t] = 1
-            m2[m2<t] = 0
-            m = m1 + m2
             s = m.shape
             #write to file all those samples that have been dropped due to unclear phenotype status
             #TODO not yet tested
@@ -67,7 +76,8 @@ def threshold_matrix(dir1, t, outdir, loss_dir2=None, is_internal = False):
             f.close()
             #combine the two conditions
             m = m[drop_m1 & drop_m2 | (m.iloc[:,m.shape[1] - 1] >= 1)]
-            print "there are %s cases with probable gain and loss event for the same edge" % (m > 1).sum().sum()
+            if discretize_pt_only:
+                print "there are %s cases with probable gain and loss event for the same edge" % (m > 1).sum().sum()
             print "there are %s probable phenotype events" % (m.iloc[:, m.shape[1] - 1] >= 1).sum()
             print "there were %s samples removed due to unclear status" % (s[0] - m.shape[0])
             #for debugging purposes only
@@ -96,10 +106,11 @@ if __name__=="__main__":
 -t <threshold> threshold to discretize the likelihood matrices i.e. transform them into actual reconstruction events i.e. m [ m > threshold ] = 1 ; m [ m < threshold] = 0 
 -o <out dir> for the discretized matrices 
 -f <optional second directory> with gain or loss likelihood matrices that shall be combined with those given in the input dir specified by the -d option
+-g if option set discretize phenotypes only
         """ % (sys.argv[0])
         sys.exit(1)
     try:
-        optlist, args = getopt.getopt(sys.argv[1:], "d:t:o:f:")
+        optlist, args = getopt.getopt(sys.argv[1:], "d:t:o:f:g")
     except getopt.GetoptError as err:
         # print help information and exit:
         print str(err)  # will print something like "option -a not recognized"
@@ -108,6 +119,7 @@ if __name__=="__main__":
     t = None
     out = None
     dir2 = None
+    discretize_pt_only = False 
     for o, a in optlist:
         if o == "-d":
             dir1 = a
@@ -122,6 +134,8 @@ if __name__=="__main__":
                 os.mkdir(out)
         if o == "-f":
             dir2 = a
-    threshold_matrix(dir1, t, out, dir2)
+        if o == "-g":
+            discretize_pt_only = True
+    threshold_matrix(dir1, t, out, dir2, discretize_pt_only = True)
     #threshold_matrix("/net/metagenomics/projects/phenotypes_20130523/gideon/mapping/stol_2_NCBI20140115_candidatus_sample30/likelihood/gain_prob", t=0.5, outdir="/net/metagenomics/projects/phenotypes_20130523/gideon/mapping/stol_2_NCBI20140115_candidatus_sample30/likelihood/gain_prob0.5/")
     #threshold_matrix("/net/metagenomics/projects/phenotypes_20130523/gideon/mapping/stol_2_NCBI20140115_candidatus_sample30/likelihood/gain_prob",loss_dir2="/net/metagenomics/projects/phenotypes_20130523/gideon/mapping/stol_2_NCBI20140115_candidatus_sample30/likelihood/loss_prob", t=0.5, outdir="/net/metagenomics/projects/phenotypes_20130523/gideon/mapping/stol_2_NCBI20140115_candidatus_sample30/likelihood/gain_loss_prob0.5/")
