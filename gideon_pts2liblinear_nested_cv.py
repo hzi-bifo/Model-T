repaired bug in the normalization xp was replace with norm(x)

gideon_pts2liblinear_nested_cv.py
@@ -88,7 +88,7 @@ def cv_and_fs(bin, model_out, gt_start, gt_end, pt_start, pt_end, phypat_f, rec_
             if likelihood_params is None:
                 x = (x > 0).astype('int')
         else:
-            x_p, nf = ncv.normalize(x.astype('double'))
+            x_p, nf = ncv.normalize(x_p.astype('double'))
             np.savetxt(fname="%s/%s_normf_xp.dat"%(model_out, pt_out), X=nf)
             x, nf = ncv.normalize(x.astype('double'))
             np.savetxt(fname="%s/%s_normf_x.dat"%(model_out, pt_out), X=nf)