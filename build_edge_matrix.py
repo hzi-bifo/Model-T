3cd43a1039ca30be5f754f0e47a35629c7c12487
Aaron Weimann <aaron.weimann@uni-duesseldorf.de>
2015-01-08 11:28:49 +0100

    removed some comments

--- build_edge_matrix.py
+++ build_edge_matrix.py
@@ -256,11 +256,9 @@ class build_edge_matrix:
         """for all phenotypes generate a edge based matrix"""
         for pt in range(pt_start, pt_end+1):
             #check if phenotype has any events associated with it
-            print "current phenotype is:", pt
             if pt not in self.char2ev:
                 print "phenotype", pt, "has no events associated with it. Skipping.."
                 continue
-            print "current phenotype number being processed", pt
             edges = self.get_edges(pt)
             pt_dict = self.get_pt_dict(edges)
             #plus one because we want to include column gt_end
