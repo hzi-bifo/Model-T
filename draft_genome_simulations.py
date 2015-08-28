3fdad7062e69f228a166842e137d7797c99f3d00
Aaron Weimann <aaron.weimann@uni-duesseldorf.de>
2015-08-28 16:09:18 +0200

    adjusted code to work with most recent version of the traitAR program, rewrote and tidied up command line interface with argparse, added adjust bias option etc.

--- draft_genome_simulations.py
+++ draft_genome_simulations.py
@@ -13,11 +13,10 @@ from scipy.stats import nanmean
 UID2FN = "/net/metagenomics/data/genomes/public/NCBI20140115_FAAs_uid2fn.txt"
 class SimulateDraftGenome:
 
-    def __init__(self, sample_points, no_samples, refseq_annot_fs, refseq_f, pfam_acc_f, model_dir, train_pfam_f, sp2str_f, min_samples):
+    def __init__(self, sample_points, no_samples, refseq_annot_fs, refseq_f, pfam_acc_f, model_tar, sp2str_f, min_samples):
         self.sample_points = sample_points
         self.no_samples = no_samples
-        self.model_dir = model_dir
-        self.train_pfam_f = train_pfam_f
+        self.model_tar = model_tar
         #minimum number of samples per phenotype + and - class 
         self.min_samples = min_samples
         fs = [i.strip() for i in open(refseq_annot_fs, 'r').readlines()]
@@ -46,10 +45,10 @@ class SimulateDraftGenome:
         return sp2gene
             
 
-    def simulate_draft_genomes(self):
+    def simulate_draft_genomes(self, pfam_acc_f):
         """create for each fully sequenced genome, x number of samples for y number of sample points"""
         #read in pfam accs
-        pfams = ps.read_csv(pfam_acc_f, index_col = 0, header = None).index
+        pfams = ps.read_csv(pfam_acc_f, header=None, index_col = 0, sep = "\t").index
         #sample Pfam matrices at different samples points
         full_m = ps.DataFrame(ps.np.zeros(shape = (len(self.sp2gene) * len(self.sample_points) * self.no_samples, len(pfams))))
         full_m.columns = pfams
@@ -73,7 +72,7 @@ class SimulateDraftGenome:
                 cur_sample_series = m.iloc[0, ].copy()
                 #draw a samples of genes with the same size 
                 genes = random.sample(self.sp2gene[self.sp2gene.keys()[j]], len(self.sp2gene[self.sp2gene.keys()[j]])) 
-                print "total number of genes", len(genes)
+                #print "total number of genes", len(genes)
                 #draw random sample
                 
                 #retrieve vector of pfams 
@@ -82,7 +81,7 @@ class SimulateDraftGenome:
                     if k + 1 >= cur_sample_point * len(genes):
                         #print i, sample_points_t, cur_sample_point
                         #if so assign that vector to the matrix
-                        print "sampling point at", cur_sample_point, k
+                        #print "sampling point at", cur_sample_point, k
                         m.loc[cur_sample_point, ] = cur_sample_series
                         #stop when all sampling points have been processed
                         if sample_points_t == []:
@@ -102,6 +101,36 @@ class SimulateDraftGenome:
         #ps.set_option("display.max_columns",101)
         return full_m
 
+    def predict_draft_genomes(self,data, pt1, pt2, k = 5, mask_pts = None, adjust_bias = False):
+        """for all the phenotypes and all simulated genomes get phenotype predictions"""
+        self.pt_has_model = dict((i, False) for i in range(pt1, pt2 + 1)) 
+        #restrict the data matrix to the pfams from the training data set
+        pfam_pts_mapping = ps.read_csv(self.model_tar.extractfile("pfam_pts_names_nl_desc.txt"), header=None, index_col = 0, sep = "\t")
+        #full_m.columns = pfam_pts_mapping.loc[full_m.columns, 0] - 1
+        data_train_pf = data.loc[:, pfam_pts_mapping.iloc[:-93, 0]]
+        data_train_pf.columns = pfam_pts_mapping.index[:-93].values - 1
+        #instaniate a matrix for the individual predictions
+        preds = ps.DataFrame(ps.np.zeros(shape = (len(self.sp2gene) * len(self.sample_points) * self.no_samples ,pt2 + 1 - pt1)))
+        preds.columns = range(pt1, pt2 + 1)
+        #print preds
+        for pt_i in range(pt2 + 1 - pt1):
+            #predict each sampling point independently to allow adjusting the bias term
+            for i in range(len(self.sample_points)):
+                data_train_pf_sp = data_train_pf.iloc[[i + j * len(self.sample_points)  for j in range(self.no_samples * len(self.sp2gene))],:]
+                if adjust_bias:
+                    maj_pred = predict.majority_predict(range(pt1, pt2 + 1)[pt_i], self.model_tar, data_train_pf_sp, k, bias_weight = self.sample_points[i])
+                else:
+                    maj_pred = predict.majority_predict(range(pt1, pt2 + 1)[pt_i], self.model_tar, data_train_pf_sp, k, bias_weight = 1)
+                maj_preds_aggr = maj_pred.apply(lambda x: 1 if (x > 0).sum() > k/2  else -1, axis = 1)
+                #if there is a model available fill in the phenotype prediction into the matrix
+                if not len(maj_preds_aggr) == 0:
+                    self.pt_has_model[range(pt1, pt2 + 1)[pt_i]] = True
+                    #set the corresponding entry in the prediction matrix, make sure that this is the value not a vector, will default to na (NASTY BUG)
+                    preds.iloc[[i + j * len(self.sample_points)  for j in range(self.no_samples * len(self.sp2gene))], pt_i] = maj_preds_aggr.values
+        if not mask_pts is None:
+            for pt in mask_pts:
+                self.pt_has_model[pt] = False
+        return preds.loc[:, [self.pt_has_model[i] for i in range(pt1, pt2 + 1)]]
    
     def evaluate_draft_genome_preds(self, preds, pt_gold_f, pt2id_f, outdir):
         """read in gold standard pt predictions"""
@@ -129,23 +158,23 @@ class SimulateDraftGenome:
         #get phenotypes that are completely absent in the gold standard
         pt_gold_absent = (~pt_gold_missing_m).apply(ps.np.all)
         #get phenotypes that have less than x number of pos samples and y number of negative samples
-        pt_gold_too_few_samples = pt_gold_sel_model.apply(lambda x: ps.Series(((x[~ps.isnull(x) & (x < 0)].sum() <= -5) & (x[~ps.isnull(x) & (x > 0)].sum() >= self.min_samples))))
+        pt_gold_too_few_samples = pt_gold_sel_model.apply(lambda x: ps.Series(((x[~ps.isnull(x) & (x < 0)].sum() <= -self.min_samples) & (x[~ps.isnull(x) & (x > 0)].sum() >= self.min_samples))))
         #print "pt_gold_absent", pt_gold_absent
         #if not pt_gold_missing_m.all().all():
         #    print "no labels found for the input phenotypes in the gold standard"
         #    sys.exit(1)
         #unfold array and index missing phenotypes
-        perf_sampling_points_pt = ps.DataFrame(ps.np.zeros(shape = (len(sampling_points), 4 * pt_gold_sel.shape[1])))
-        perf_sampling_points_pt.index = sampling_points
+        perf_sampling_points_pt = ps.DataFrame(ps.np.zeros(shape = (len(self.sample_points), 4 * pt_gold_sel.shape[1])))
+        perf_sampling_points_pt.index = self.sample_points
         #perf_sampling_points.columns = ["recall positive class", "recall negative class", "balanced accuracy", "precision"]
-        for i in range(len(sampling_points)):
-            print "processing sampling point", sampling_points[i]
+        for i in range(len(self.sample_points)):
+            #print "processing sampling point", sampling_points[i]
             #get confusion matrix at each sampling point i.e. 10%, 20%, ..., 100%
             #matrix to store the confusion matrices for each phenotype and sub sample
-            perf_sample_pt  = ps.DataFrame(ps.np.zeros(shape = (no_samples, 4 * pt_gold_sel.shape[1])))
-            for j in range(no_samples):
+            perf_sample_pt  = ps.DataFrame(ps.np.zeros(shape = (self.no_samples, 4 * pt_gold_sel.shape[1])))
+            for j in range(self.no_samples):
                 #get indices  for each species at the same sampling point for the same sub sample
-                loc_pred = [i + len(sampling_points) * j + len(sampling_points) * no_samples * k for k in range(len(self.sp2gene))] 
+                loc_pred = [i + len(self.sample_points) * j + len(self.sample_points) * self.no_samples * k for k in range(len(self.sp2gene))] 
                 labels = pt_gold_df 
                 #print "sampling_point", sampling_points[i], "sample", j
                 #print "loc_pred", loc_pred
@@ -163,12 +192,12 @@ class SimulateDraftGenome:
             #print "perf_sample_pt sample point", i, perf_sample_pt.sum(axis = 1)
         #print "perf_sampling_points_pt", perf_sampling_points_pt
         #micro accuracy
-        conf_per_sp = ps.DataFrame(ps.np.zeros(shape = (len(sampling_points), 4)))
-        conf_per_sp.index = sampling_points
+        conf_per_sp = ps.DataFrame(ps.np.zeros(shape = (len(self.sample_points), 4)))
+        conf_per_sp.index = self.sample_points
         conf_per_sp.columns = ["TN", "FP", "FN", "TP"]
         #store performance measures per sampling point per phenotype
-        perf_per_pt = ps.np.zeros(shape = (len(sampling_points), pt_gold_sel_model.shape[1], 4))
-        for sp in range(len(sampling_points)):
+        perf_per_pt = ps.np.zeros(shape = (len(self.sample_points), pt_gold_sel_model.shape[1], 4))
+        for sp in range(len(self.sample_points)):
             for pt_i in range(pt_gold_sel_model.shape[1]):
                 conf_pt = perf_sampling_points_pt.iloc[sp, pt_i * 4 : (pt_i * 4 + 4)]
                 #print "conf_pt", conf_pt
@@ -183,9 +212,8 @@ class SimulateDraftGenome:
         #print "perf_per_pt", perf_per_pt
         perf_colnames = ["recall positive class", "recall negative class", "balanced accuracy", "precision"]
         #get macro measures 
-        print ((~pt_gold_absent).values & pt_gold_too_few_samples).values
         macro_per_sp = ps.DataFrame(nanmean(perf_per_pt[:, ((~pt_gold_absent) & pt_gold_too_few_samples).values[0], : ], axis = 1))
-        macro_per_sp.index = sampling_points
+        macro_per_sp.index = self.sample_points
         macro_per_sp.columns = perf_colnames 
         #get micro measures 
         micro_per_sp = conf_per_sp.apply(lambda x: ps.Series([nested_cv.nested_cv.recall_pos_conf(x), nested_cv.nested_cv.recall_neg_conf(x), nested_cv.nested_cv.bacc(nested_cv.nested_cv.recall_pos_conf(x), nested_cv.nested_cv.recall_neg_conf(x)), nested_cv.nested_cv.precision_conf(x)]), axis = 1)
@@ -195,45 +223,23 @@ class SimulateDraftGenome:
         micro_per_sp.to_csv("%s/micro.tsv"%outdir, sep = "\t")
         macro_per_sp.to_csv("%s/macro.tsv"%outdir, sep = "\t")
         #write individual phenotype performances to disk per sampling point
-        for i in range(len(sampling_points)):
+        for i in range(len(self.sample_points)):
             df = ps.DataFrame(perf_per_pt[i, :, :])
             df.index = pt_gold_sel_model.columns
             df.columns = perf_colnames
-            df[~pt_gold_absent].to_csv("%s/perf_sp%s.tsv"%(outdir, sampling_points[i]), sep = "\t")
+            df[~pt_gold_absent].to_csv("%s/perf_sp%s.tsv"%(outdir, self.sample_points[i]), sep = "\t")
 
 
          
                 
 
-    def predict_draft_genomes(self,data, pt1, pt2, k = 5, mask_pts = None):
-        """for all the phenotypes and all simulated genomes get phenotype predictions"""
-        self.pt_has_model = dict((i, False) for i in range(pt1, pt2 + 1)) 
-        #restrict the data matrix to the pfams from the training data set
-        data_train_pf = data.loc[:, ps.read_csv(self.train_pfam_f, index_col = 0, header = None).index]
-        #instaniate a matrix for the individual predictions
-        preds = ps.DataFrame(ps.np.zeros(shape = (len(self.sp2gene) * len(self.sample_points) * self.no_samples ,pt2 + 1 - pt1)))
-        preds.columns = range(pt1, pt2 + 1)
-        print preds
-        for pt in range(pt1, pt2 + 1):
-            maj_pred = predict.majority_predict(pt, self.model_dir, data_train_pf, k)
-            maj_preds_aggr = maj_pred.apply(lambda x: 1 if (x > 0).sum() > k/2  else -1, axis = 1)
-            #if there is a model available fill in the phenotype prediction into the matrix
-            if not len(maj_preds_aggr) == 0:
-                self.pt_has_model[pt] = True
-                #set the corresponding entry in the prediction matrix, make sure that this is the value not a vector, will default to na (NASTY BUG)
-                preds.loc[:, pt] = maj_preds_aggr.values
-                print pt, maj_preds_aggr, "\n", preds.loc[:, pt]
-        if not mask_pts is None:
-            for pt in mask_pts:
-                self.pt_has_model[pt] = False
-        return preds.loc[:, [self.pt_has_model[i] for i in range(pt1, pt2 + 1)]]
 
     def get_genes_targz(self, gff_targz):
         rs2gene = {}
         #read refseq id (uid) to directory mapping
         uid2fn = ps.read_csv(UID2FN, sep = "\t", index_col = 0, header = None) 
         uid2fn.index = ["RefSeqProId+%s" % i for i in uid2fn.index]
-        print self.sp2gene
+        #print self.sp2gene
         with tarfile.open(gff_targz, mode = 'r:gz') as tf:
             for rs in self.gene2hmm:
                 for ce in self.rs2f[rs]:
@@ -244,71 +250,33 @@ class SimulateDraftGenome:
 
  
 if __name__ == '__main__':
-    import getopt
-    if len(sys.argv) == 1:
-        print """USAGE: python %s
--s <number of samples> number of samples to be drawn for each genome
--p <[sampling point 1, sampling point 2, ...]> sequence of ordered relative number of genes to be retained e.g. -p 0.1,0.2,0.5,1.0 
--g Pfam (gene/protein) family accession files
--f <gene counts> which contains a bunch of gff file for every species
--m <model dir> directory with the liblinear models 
--a <file> with refseq chromosomal annotation files 
--r <file> with refseq ids 
--b <phenotype_range> e.g. 5000:5010
--c <gold standard matrix> with the phenotypes
--d <pfams> that occur in the training set 
--i <mapping file> that maps phenotypes to the internally used ids
--e <mapping file> that maps RefSeqProIds to species
--o <outdir> for macro, micro accuracy etc. 
--n <number> of samples required for the phenotype + and - class to be considered in the macro accuracy computation
-""" % (sys.argv[0])
-        sys.exit(1)
-    try:
-        optlist, args = getopt.getopt(sys.argv[1:], "s:p:g:m:r:a:b:c:d:i:e:o:k:n:")
-    except getopt.GetoptError as err:
-        # print help information and exit:
-        print str(err)  # will print something like "option -a not recognized"
-        sys.exit(2)
-    no_samples = sampling_points = gff_targz = pfam_acc_f = model_dir = refseq_annot_fs = refseq_f = tr_pfams_f = pt_gold_f = pt2id_f = sp2str_f = outdir = mask_pts = min_samples = None
-    for o, a in optlist:
-        if o == "-b":
-            pt1, pt2 = [int(i) for i in a.split("-")]
-        if o == "-s":
-            no_samples = int(a)
-        if o == "-p":
-            sampling_points = [float(i) for i in a.split(",")] 
-        if o == "-g":
-            pfam_acc_f = a
-        #if o == "-f":
-        #    gene_length_f = a
-        if o == "-m":
-            model_dir = a
-        if o == "-a":
-            refseq_annot_fs = a
-        if o == "-r":
-            refseq_f = a
-        if o == "-c":
-            pt_gold_f = a
-        if o == "-d":
-            tr_pfams_f = a
-        if o == "-i":
-            pt2id_f = a
-        if o == "-e":
-            sp2str_f = a
-        if o == "-k":
-            mask_pts = [int(i) for i in a.split(",")]
-        if o == "-o":
-            outdir = a
-        if o == "-n":
-            min_samples = int(a)
+    import argparse
+    parser = argparse.ArgumentParser("combine the misclassified samples of different phenotypes into data matrices")
+    parser.add_argument("sample_points", help='<[sampling point 1, sampling point 2, ...]> sequence of ordered relative number of genes to be retained e.g. -p 0.1,0.2,0.5,1.0')
+    parser.add_argument("pfam_acc_f", help='Pfam (gene/protein) family accession files')
+    parser.add_argument("no_samples", help='number of samples to be drawn for each genome', type = int)
+    parser.add_argument("model_tar", help='archive with the liblinear models')
+    parser.add_argument("refseq_annot_fs", help='directory with refseq chromosomal annotation files ')
+    parser.add_argument("refseq_id_f", help='file with refseq ids')
+    parser.add_argument("pt_range", help='range of phenotypes e.g. 8476-8568')
+    parser.add_argument("pt_gold_f", help='gold standard matrix with the phenotypes')
+    parser.add_argument("pt2id_f", help='out_dir for macro, micro accuracy etc.')
+    parser.add_argument("outdir", help='out_dir for macro, micro accuracy etc.')
+    parser.add_argument("sp2str_f", help='mapping file that maps RefSeqProIds to species')
+    parser.add_argument("min_samples", help='number of samples required for the phenotype + and - class to be considered in the macro accuracy computation', type = int)
+    parser.add_argument("mask_pts", help='mask those phenotypes e.g. 8476,8521,8533')
+    parser.add_argument("--adjust_bias", help='if set adjust the bias term according to the percentage of genes retained', action = 'store_true')
+    args = parser.parse_args()
+    pt1, pt2 = [int(i) for i in args.pt_range.split("-")]
+    args.sample_points = [float(i) for i in args.sample_points.split(",")] 
     import os
-    if os.path.exists(outdir):
-        sys.stderr.write("outdir %s already exists"%outdir)
+    if os.path.exists(args.outdir):
+        sys.stderr.write("outdir %s already exists"%args.outdir)
         sys.exit(1)
     else:
-        os.mkdir(outdir)
-
-    simulator = SimulateDraftGenome(sampling_points, no_samples, refseq_annot_fs, refseq_f, pfam_acc_f,  model_dir, tr_pfams_f, sp2str_f, min_samples)
-    data = simulator.simulate_draft_genomes()
-    preds = simulator.predict_draft_genomes(data, pt1, pt2, 5, mask_pts)
-    simulator.evaluate_draft_genome_preds(preds, pt_gold_f, pt2id_f, outdir)
+        os.mkdir(args.outdir)
+    
+    simulator = SimulateDraftGenome(args.sample_points, args.no_samples, args.refseq_annot_fs, args.refseq_id_f, args.pfam_acc_f,  tarfile.open(args.model_tar, mode = "r:gz"),  args.sp2str_f, args.min_samples)
+    data = simulator.simulate_draft_genomes(args.pfam_acc_f)
+    preds = simulator.predict_draft_genomes(data, pt1, pt2, 5, args.mask_pts, args.adjust_bias)
+    simulator.evaluate_draft_genome_preds(preds, args.pt_gold_f, args.pt2id_f, args.outdir)
